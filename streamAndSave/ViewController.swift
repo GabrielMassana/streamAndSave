964932c70f4e4fac3880be60d6dd9a15c13c6fe2
GabrielMassana <gabrielmassana@gmail.com>
2016-06-02 17:44:47 +0100

    add empty project

---
+++ streamAndSave/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  streamAndSave
+//
+//  Created by GabrielMassana on 02/06/2016.
+//  Copyright © 2016 GabrielMassana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
